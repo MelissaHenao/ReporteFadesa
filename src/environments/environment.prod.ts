feff81c71a83164c70ca19f27a14c52522e04014
Melissa Henao Vahos <98412920+MelissaHenao@users.noreply.github.com>
2022-12-19 09:15:51 -0500

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  webHockUrl:"https://fadesa.bitrix24.es/rest/105/72gz9s1w6spw04y5"
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  webHockUrl:"https://fadesa.bitrix24.es/rest/105/72gz9s1w6spw04y5"
-};
